9cb6a7746733a318cf319809e598a4225d867c77
lightyrs <harris@harrisnovick.com>
2011-05-27 18:53:16 -0400

    Initial Commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sketchysquare::Application.config.secret_token = 'a757767a8a91f2be2c313b23927413239d0e445899fc8fe1002402ec4a24f2e7f7dd90a3b0d5ad193dda7bd7be44923335482bfc1ddbc6db953c981b3856c30f'
